1ab9a6710fa4de5016416dc8928b081698b61b28
ChingT <j60626j@gmail.com>
2023-10-03 16:04:01 +0200

    Add script to create test data

---
+++ app/add_data.py
@@ -0,0 +1,131 @@
+import os
+
+# Configure the Django settings
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.cookbook_management.settings")
+import django
+
+django.setup()
+
+from django.contrib.auth.models import User
+
+from cookbook.models import Cookbook
+from ingredient.models import Ingredient
+from recipe.models import Recipe
+
+
+def create_user(username, password):
+    user = User.objects.create_user(username=username, password=password)
+    return user
+
+
+def create_ingredient(name):
+    ingredient = Ingredient.objects.create(name=name)
+    return ingredient
+
+
+def create_cookbook(title, author, description=""):
+    cookbook = Cookbook.objects.create(
+        title=title, author=author, description=description
+    )
+    return cookbook
+
+
+def create_recipe(title, cookbook, author, difficulty, description, ingredients):
+    recipe = Recipe.objects.create(
+        title=title,
+        cookbook=cookbook,
+        author=author,
+        difficulty=difficulty,
+        description=description,
+    )
+    recipe.ingredients.set(ingredients)
+    return recipe
+
+
+def add_favorite_recipe(recipe, user):
+    recipe.favourite_by_users.add(user)
+
+
+def add_favorite_cookbook(cookbook, user):
+    cookbook.favourite_by_users.add(user)
+
+
+if __name__ == "__main__":
+    User.objects.exclude(id=1).delete()
+    Ingredient.objects.all().delete()
+
+    # Create users
+    user1 = create_user("Peter", "0000")
+    user2 = create_user("Alice", "0000")
+    user3 = create_user("John", "0000")
+
+    # Create ingredients
+    flour = create_ingredient("Flour")
+    eggs = create_ingredient("Eggs")
+    milk = create_ingredient("Milk")
+    sugar = create_ingredient("Sugar")
+    salt = create_ingredient("Salt")
+    butter = create_ingredient("Butter")
+    bacon = create_ingredient("Bacon")
+    parmesan = create_ingredient("Parmesan Cheese")
+    black_pepper = create_ingredient("Black Pepper")
+    spaghetti = create_ingredient("Spaghetti")
+    pepper = create_ingredient("Pepper")
+
+    # Create cookbooks
+    cookbook1 = create_cookbook(
+        "Baking Delights", user1, "A cookbook for delicious baked goods."
+    )
+    cookbook2 = create_cookbook(
+        "Healthy Eating", user2, "Discover healthy and nutritious recipes."
+    )
+
+    # Create recipes
+    recipe1 = create_recipe(
+        "Chocolate Cake",
+        cookbook1,
+        user1,
+        Recipe.Difficulty.Hard,
+        "A delicious chocolate cake recipe.",
+        [flour, eggs, milk, sugar, butter],
+    )
+
+    recipe2 = create_recipe(
+        "Pancakes",
+        cookbook1,
+        user1,
+        Recipe.Difficulty.Easy,
+        "Fluffy pancakes for breakfast.",
+        [flour, eggs, milk, butter, sugar],
+    )
+
+    recipe3 = create_recipe(
+        "Spaghetti Carbonara",
+        cookbook2,
+        user2,
+        Recipe.Difficulty.Intermediate,
+        "Classic Italian pasta dish.",
+        [eggs, bacon, parmesan, black_pepper, spaghetti],
+    )
+
+    recipe4 = create_recipe(
+        "Omelette",
+        cookbook2,
+        user3,
+        Recipe.Difficulty.Intermediate,
+        "Quick and simple omelette recipe.",
+        [eggs, butter, salt, pepper],
+    )
+
+    # Add favorite recipes and cookbooks for users
+    add_favorite_recipe(recipe1, user1)
+    add_favorite_recipe(recipe1, user3)
+    add_favorite_recipe(recipe2, user2)
+    add_favorite_recipe(recipe3, user1)
+    add_favorite_recipe(recipe4, user3)
+    add_favorite_cookbook(cookbook1, user1)
+    add_favorite_cookbook(cookbook1, user3)
+    add_favorite_cookbook(cookbook2, user2)
+    add_favorite_cookbook(cookbook2, user3)
+
+    print("Test data added successfully!")
