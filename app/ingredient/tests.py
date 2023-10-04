cacfa5e1b5624de013a769e4a51a9cfce489e20e
ChingT <j60626j@gmail.com>
2023-10-04 16:59:26 +0200

    Improve tests

--- app/ingredient/tests.py
+++ app/ingredient/tests.py
@@ -1,21 +1,26 @@
+from django.contrib.auth import get_user_model
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
 from ingredient.models import Ingredient
 
+User = get_user_model()
+
 
 class IngredientTests(APITestCase):
     def setUp(self):
-        self.ingredient_data = {"name": "Test Ingredient"}
-        self.ingredient = Ingredient.objects.create(**self.ingredient_data)
+        author = User.objects.create_user("test_user", password="0000")
+        ingredient_data = {"name": "Test Ingredient"}
+        ingredient = Ingredient.objects.create(**ingredient_data)
+        self.created_data = {"id": ingredient.id, "name": ingredient.name}
 
     def test_get_all_ingredients(self):
         url = reverse("ingredient-list")
         response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(Ingredient.objects.count(), 1)
-        self.assertEqual(Ingredient.objects.get().name, self.ingredient.name)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0], self.created_data)
 
     def test_create_ingredient(self):
         url = reverse("ingredient-list")
@@ -24,23 +29,26 @@ class IngredientTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Ingredient.objects.count(), 2)
         created_ingredient = Ingredient.objects.get(id=response.data["id"])
-        self.assertEqual(created_ingredient.name, ingredient_data["name"])
+        created_ingredient_data = {"name": created_ingredient.name}
+        self.assertEqual(created_ingredient_data, ingredient_data)
 
     def test_get_ingredient(self):
-        url = reverse("ingredient-detail", args=[self.ingredient.id])
+        url = reverse("ingredient-detail", args=[self.created_data["id"]])
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(Ingredient.objects.get().name, self.ingredient.name)
+        self.assertEqual(response.data, self.created_data)
 
     def test_update_ingredient(self):
-        url = reverse("ingredient-detail", args=[self.ingredient.id])
+        url = reverse("ingredient-detail", args=[self.created_data["id"]])
         updated_data = {"name": "Updated Ingredient"}
         response = self.client.patch(url, updated_data, format="json")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(Ingredient.objects.get().name, updated_data["name"])
+        self.assertEqual(Ingredient.objects.count(), 1)
+        updated_ingredient = Ingredient.objects.get(id=response.data["id"])
+        self.assertEqual(updated_ingredient.name, updated_data["name"])
 
     def test_delete_ingredient(self):
-        url = reverse("ingredient-detail", args=[self.ingredient.id])
+        url = reverse("ingredient-detail", args=[self.created_data["id"]])
         response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-        self.assertFalse(Ingredient.objects.filter(id=self.ingredient.id).exists())
+        self.assertFalse(Ingredient.objects.filter(id=self.created_data["id"]).exists())
--- app/recipe/tests.py
+++ app/recipe/tests.py
@@ -14,52 +14,69 @@ class RecipeTests(APITestCase):
         author = User.objects.create_user("test_user", password="0000")
         cookbook_data = {"title": "Test Cookbook", "author": author}
         cookbook = Cookbook.objects.create(**cookbook_data)
-        self.recipe_data = {
+        recipe_data = {
             "title": "Test Recipe",
+            "description": "Test description",
+            "difficulty": 1,
             "author": author,
             "cookbook": cookbook,
         }
-        self.recipe = Recipe.objects.create(**self.recipe_data)
+        recipe = Recipe.objects.create(**recipe_data)
+        self.created_data = {
+            "id": recipe.id,
+            "title": recipe.title,
+            "description": recipe.description,
+            "difficulty": recipe.difficulty,
+            "author": recipe.author.id,
+            "cookbook": recipe.cookbook.id,
+        }
 
     def test_get_all_recipes(self):
         url = reverse("recipe-list")
         response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(Recipe.objects.count(), 1)
-        self.assertEqual(Recipe.objects.get().title, self.recipe.title)
-        self.assertEqual(Recipe.objects.get().author, self.recipe.author)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0], self.created_data)
 
     def test_create_recipe(self):
         url = reverse("recipe-list")
         recipe_data = {
-            "title": "Test Recipe",
-            "author": self.recipe_data["author"].id,
-            "cookbook": self.recipe_data["cookbook"].id,
+            "title": "New Recipe",
+            "description": "New description",
+            "difficulty": 2,
+            "author": self.created_data["author"],
+            "cookbook": self.created_data["cookbook"],
         }
         response = self.client.post(url, recipe_data, format="json")
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Recipe.objects.count(), 2)
         created_recipe = Recipe.objects.get(id=response.data["id"])
-        self.assertEqual(created_recipe.title, recipe_data["title"])
-        self.assertEqual(created_recipe.author.id, recipe_data["author"])
+        created_recipe_data = {
+            "title": created_recipe.title,
+            "description": created_recipe.description,
+            "difficulty": created_recipe.difficulty,
+            "author": created_recipe.author.id,
+            "cookbook": created_recipe.cookbook.id,
+        }
+        self.assertEqual(created_recipe_data, recipe_data)
 
     def test_get_recipe(self):
-        url = reverse("recipe-detail", args=[self.recipe.id])
+        url = reverse("recipe-detail", args=[self.created_data["id"]])
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(Recipe.objects.get().title, self.recipe.title)
-        self.assertEqual(Recipe.objects.get().author, self.recipe.author)
+        self.assertEqual(response.data, self.created_data)
 
     def test_update_recipe(self):
-        url = reverse("recipe-detail", args=[self.recipe.id])
+        url = reverse("recipe-detail", args=[self.created_data["id"]])
         updated_data = {"title": "Updated Recipe"}
         response = self.client.patch(url, updated_data, format="json")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(Recipe.objects.get().title, updated_data["title"])
-        self.assertEqual(Recipe.objects.get().author, self.recipe.author)
+        self.assertEqual(Recipe.objects.count(), 1)
+        updated_recipe = Recipe.objects.get(id=response.data["id"])
+        self.assertEqual(updated_recipe.title, updated_data["title"])
 
     def test_delete_recipe(self):
-        url = reverse("recipe-detail", args=[self.recipe.id])
+        url = reverse("recipe-detail", args=[self.created_data["id"]])
         response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-        self.assertFalse(Recipe.objects.filter(id=self.recipe.id).exists())
+        self.assertFalse(Recipe.objects.filter(id=self.created_data["id"]).exists())
